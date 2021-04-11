7873919f54b5c49396346b07f5f9f851f7d13689
Akshat Kumar <kumarakshat4596@gmail.com>
2021-04-11 19:57:53 +0530

    Created NoteDao and NoteDatabase to store note objects in the database

---
+++ app/src/main/java/com/example/notekeeper/database/NoteDatabase.java
@@ -0,0 +1,30 @@
+package com.example.notekeeper.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.notekeeper.model.Note;
+
+@Database(entities = Note.class, version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+
+    private static NoteDatabase instance;
+
+    public abstract NoteDao noteDao();
+
+    public static NoteDatabase getInstance(Context context){
+        synchronized (NoteDatabase.class){
+            if(instance == null){
+                instance = Room.databaseBuilder(context.getApplicationContext(),
+                        NoteDatabase.class, "note_database")
+                        .fallbackToDestructiveMigration()
+                        .build();
+            }
+        }
+        return instance;
+    }
+
+}
