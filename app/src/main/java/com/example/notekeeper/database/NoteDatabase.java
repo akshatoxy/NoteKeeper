e18ee3fc650afc40d44b8026fc3c7b8e42062194
Akshat Kumar <kumarakshat4596@gmail.com>
2021-04-11 20:28:16 +0530

    Added noterepository class to manage all the database operation and provide an abstraction between the viewmodel and room database

--- app/src/main/java/com/example/notekeeper/database/NoteDatabase.java
+++ app/src/main/java/com/example/notekeeper/database/NoteDatabase.java
@@ -1,10 +1,13 @@
 package com.example.notekeeper.database;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.notekeeper.model.Note;
 
@@ -21,10 +24,38 @@ public abstract class NoteDatabase extends RoomDatabase {
                 instance = Room.databaseBuilder(context.getApplicationContext(),
                         NoteDatabase.class, "note_database")
                         .fallbackToDestructiveMigration()
+                        .addCallback(roomCallback) // To populate the database when created
                         .build();
             }
         }
         return instance;
+    }
+
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbTask extends AsyncTask<Void, Void, Void>{
+        private NoteDao noteDao;
+
+        private PopulateDbTask(NoteDatabase db){
+            this.noteDao = db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title 1", "Description 1", 1));
+            noteDao.insert(new Note("Title 2", "Description 2", 2));
+            noteDao.insert(new Note("Title 3", "Description 3", 3));
+            noteDao.insert(new Note("Title 4", "Description 4", 4));
+            noteDao.insert(new Note("Title 5", "Description 5", 5));
+            return null;
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/notekeeper/repository/NoteRepository.java
@@ -0,0 +1,99 @@
+package com.example.notekeeper.repository;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.notekeeper.database.NoteDao;
+import com.example.notekeeper.database.NoteDatabase;
+import com.example.notekeeper.model.Note;
+
+import java.util.List;
+
+public class NoteRepository {
+    private NoteDao noteDao;
+    private LiveData<List<Note>> allNotes;
+
+    public NoteRepository(Application application) {
+        NoteDatabase noteDatabase = NoteDatabase.getInstance(application);
+        this.noteDao = noteDatabase.noteDao();
+        this.allNotes = noteDao.getAllNotes();
+    }
+
+    public void insert(Note note){
+        new InsertNoteTask(noteDao).execute(note);
+    }
+
+    public void update(Note note){
+        new UpdateNoteTask(noteDao).execute(note);
+    }
+
+    public void delete(Note note){
+        new DeleteNoteTask(noteDao).execute(note);
+    }
+
+    public void deleteAll(){
+        new DeleteAllNotesTask(noteDao).execute();
+    }
+
+    public LiveData<List<Note>> getAllNotes() {
+        return allNotes;
+    }
+
+    private static class InsertNoteTask extends AsyncTask<Note, Void, Void>{
+        private NoteDao noteDao;
+
+        private InsertNoteTask(NoteDao noteDao){
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.insert(notes[0]);
+            return null;
+        }
+    }
+
+    private static class UpdateNoteTask extends AsyncTask<Note, Void, Void>{
+        private NoteDao noteDao;
+
+        private UpdateNoteTask(NoteDao noteDao){
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.update(notes[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteNoteTask extends AsyncTask<Note, Void, Void>{
+        private NoteDao noteDao;
+
+        private DeleteNoteTask(NoteDao noteDao){
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.delete(notes[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteAllNotesTask extends AsyncTask<Void, Void, Void>{
+        private NoteDao noteDao;
+
+        private DeleteAllNotesTask(NoteDao noteDao){
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.deleteAllNotes();
+            return null;
+        }
+    }
+}
